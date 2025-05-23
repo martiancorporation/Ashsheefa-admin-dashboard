b23f338963e45c0d014e0f209f6c93b8dbe60148
Molla1441 <mhasim1441@gmail.com>
2025-05-23 19:32:19 +0530

    feat: login form update

--- src/app/components/LoginForm.jsx
+++ src/app/components/LoginForm.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { toast } from "sonner";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function LoginForm() {
     const [buttonLoading, setButtonLoading] = useState(false);
@@ -99,38 +100,39 @@ export default function LoginForm() {
                     </div>
                 </div>
 
-                <Button
-                    type="submit"
-                    className="w-full bg-[#005CD4] rounded-[6px] hover:bg-blue-600 transition-all max-w-sm h-11"
-                >
-                    {buttonLoading ? (
-                        <div className="flex items-center gap-1">
-                            <svg
-                                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                            >
-                                <circle
-                                    className="opacity-25"
-                                    cx="12"
-                                    cy="12"
-                                    r="10"
-                                    stroke="currentColor"
-                                    strokeWidth="4"
-                                ></circle>
-                                <path
-                                    className="opacity-75"
-                                    fill="currentColor"
-                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                ></path>
-                            </svg>
-                            Loading
-                        </div>
-                    ) : (
-                        "Log In"
-                    )}
-                </Button>
+                
+                <Link href={'/dashboard'}
+                        className="w-full bg-[#005CD4] flex justify-center items-center text-white rounded-[6px] hover:bg-blue-600 transition-all max-w-sm h-11"
+                    >
+                        {buttonLoading ? (
+                            <div className="flex items-center gap-1">
+                                <svg
+                                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <circle
+                                        className="opacity-25"
+                                        cx="12"
+                                        cy="12"
+                                        r="10"
+                                        stroke="currentColor"
+                                        strokeWidth="4"
+                                    ></circle>
+                                    <path
+                                        className="opacity-75"
+                                        fill="currentColor"
+                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                    ></path>
+                                </svg>
+                                Loading
+                            </div>
+                        ) : (
+                            "Log In"
+                        )}
+                    </Link>
+                
             </div>
         </form>
     );
